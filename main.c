0cf87e16b65eeb44af265093e2f6069339ca29a1
Aleks1y <74914340+Aleks1y@users.noreply.github.com>
2021-10-14 12:46:47 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define BUF_SIZE 256
+#define SUCCESS 0
+#define ERROR 1
+
+void print_text(char* str) {
+  for(int i = 0; i < 10; i++)	{
+    printf("%s %d\n", str, i);
+  }
+}
+
+void *thread_body(void *param) { 
+  print_text("Child");
+  return NULL;
+}
+
+int main(int argc, char *argv[]) { 
+  pthread_t thread; 
+  int code;
+  
+  code = pthread_create(&thread, NULL, thread_body, NULL); 
+  if (code! = SUCCESS)	{
+    perror("Creating thread error");
+    exit(ERROR);
+  }
+  
+  code = pthread_join(thread, NULL);
+  if (code != SUCCESS) {
+      perror("Thread join error");
+  }
+  print_text("Parent"); 
+  pthread_exit(NULL);
+}
